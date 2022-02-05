fc959d3d996f2495b64d310c52831cbe52e18c17
Arvid <arvid.sandin@gmail.com>
2022-02-05 11:46:12 +0100

    Fix for navbar in mobile view

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -13,9 +13,7 @@ const NavigationBar = () => {
       <NavBarItem link='/custom-top-sort' id='custom-top-sortLink' text='Custom top sort'></NavBarItem>
       <NavBarItem link='/draw-contest' id='drawcontestLink' text='Draw contest'></NavBarItem>
       <NavBarItem link='/button' id='buttonLink' text='Button'></NavBarItem>
-      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>
-        ☀/☾
-      </button>
+      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>☀/☾</button>
     </div>
   );
 };
