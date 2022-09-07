c6df48b2cb8b24e3cb19952f9b64b3c1e44833de
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2022-09-07 18:26:36 +0200

    Update content in draw-contest page

--- src/pages/draw-contest.js
+++ src/pages/draw-contest.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
 import NavigationBar from '../components/navigationbar'
 import Wrapper from '../components/wrapper'
 import Footer from '../components/footer'
@@ -7,12 +8,17 @@ const IndexPage = () => {
       <div>
         <title>Arvid Sandin - Draw contest</title>
         <NavigationBar></NavigationBar>
-        <Wrapper header={<a href=" https://drawcontest.herokuapp.com/" target='_blank' rel='noopener noreferrer'>Draw contest</a>} content={[
+        <Wrapper header={'Draw contest'} content={[
         <b>Draw contest</b>,
-        " is a real-time draw-and-guess game where one player tries to draw an image of something, while the others try to guess what it is. If a player guesses the correct word, both that player and the drawer gets awarded with one point each. The game can be played ",
-        <a href="https://drawcontest.herokuapp.com/">here</a>,
-        " and the source code is available on ",
-          <a href="https://github.com/arvidsandin/draw-contest" target='_blank' rel='noopener noreferrer'>Github</a>]}>
+        " is a real-time draw-and-guess game where one player tries to draw an image of something, while the others try to guess what it is. If a player guesses the correct word, both that player and the drawer gets awarded with one point each. Due to the removal of Heroku free plans in November 28, 2022, there is no longer a live version of this project. However, the source code is still available on ",
+          <a href="https://github.com/arvidsandin/draw-contest" target='_blank' rel='noopener noreferrer'>Github</a>,
+          <br />, <br />,
+          <StaticImage
+            src='../images/draw-contest.png'
+            alt='Screenshot of the website'
+          >
+          </StaticImage>,
+        ]}>
         </Wrapper>
         <Footer></Footer>
       </div>
