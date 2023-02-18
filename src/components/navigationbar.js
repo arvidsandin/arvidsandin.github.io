15b1940330374a3ce11362e226a9806ca7007811
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-02-18 17:30:32 +0100

    Remove button entry from navigation bar

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -12,7 +12,6 @@ const NavigationBar = () => {
       <NavBarItem link='/colorCodes' id='colorcodesLink' text='Color Codes'></NavBarItem>
       <NavBarItem link='/custom-top-sort' id='custom-top-sortLink' text='Custom top sort'></NavBarItem>
       <NavBarItem link='/draw-contest' id='drawcontestLink' text='Draw contest'></NavBarItem>
-      <NavBarItem link='/button' id='buttonLink' text='Button'></NavBarItem>
       <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>☀/☾</button>
     </div>
   );
--- src/pages/button.js
+++ src/pages/button.js
@@ -1,14 +1,10 @@
 import * as React from 'react'
-import NavigationBar from '../components/navigationbar'
-import Footer from '../components/footer'
 import TheButton from '../components/thebutton'
 const IndexPage = () => {
     return (
       <div>
         <title>Button</title>
-        <NavigationBar></NavigationBar>
         <TheButton></TheButton>
-        <Footer></Footer>
       </div>
     );
 };
