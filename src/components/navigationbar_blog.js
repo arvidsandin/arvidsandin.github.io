662b2160401a76667f72a003baff69fd743d409a
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-02-22 18:02:29 +0100

    Update dark mode toggle on all navigation bars

--- src/components/navigationbar_blog.js
+++ src/components/navigationbar_blog.js
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import NavBarItem from './navbaritem'
 import useDarkMode from 'use-dark-mode';
-import { navigationBar, button } from './navigationbar.module.css'
+import Icon from './icon'
+import Sun from '../icons/weather-1.svg'
+import Moon from '../icons/weather-114.svg'
+import { navigationBar, button, lightDarkModeIcon } from './navigationbar.module.css'
 
 const NavigationBarBlog = () => {
   const darkMode = useDarkMode(false);
@@ -9,7 +12,11 @@ const NavigationBarBlog = () => {
   return (
     <div className={navigationBar}>
       <NavBarItem link='/' id='homelink' text='Portfolio'></NavBarItem>
-      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>☀/☾</button>
+      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>
+        <Icon icon={Sun} className={lightDarkModeIcon} size='24px' alt='Change to light mode'></Icon>
+        <div className={lightDarkModeIcon} > / </div>
+        <Icon icon={Moon} className={lightDarkModeIcon} size='18px' alt='Change to dark mode'></Icon>
+      </button>
     </div>
   );
 };
