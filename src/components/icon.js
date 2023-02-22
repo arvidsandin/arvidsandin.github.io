0ee1f4a7aa9d7457ec1997d75bc0939ed68a892b
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-02-22 18:00:11 +0100

    More consistent look of dark mode toggle

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import { icon, iconClickable } from '../components/icon.module.css'
 
+const defaultSize = 32;
 
 const Logo = (props) => {
-    const size = props.size || 32
+    const height = props.height || props.size || defaultSize;
+    const width = props.width || props.size || defaultSize;
     if (props.link) {
         return (
             <a href={props.link} className={`${icon} ${iconClickable}`}>
                 <img
                     src={props.icon}
-                    style={{ height: size, width: size }}
+                    style={{ height: height, width: width }}
                     alt={props.alt}
                 />
             </a>
@@ -20,7 +22,7 @@ const Logo = (props) => {
             <div className={icon}>
                 <img
                     src={props.icon}
-                    style={{ height: size, width: size }}
+                    style={{ height: height, width: width }}
                     alt={props.alt}
                 />
             </div>
--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import NavBarItem from './navbaritem'
 import useDarkMode from 'use-dark-mode';
-import { navigationBar, button } from './navigationbar.module.css'
+import Icon from './icon'
+import Sun from '../icons/weather-1.svg'
+import Moon from '../icons/weather-114.svg'
+import { navigationBar, button, lightDarkModeIcon } from './navigationbar.module.css'
 
 const NavigationBar = () => {
   const darkMode = useDarkMode(false);
@@ -12,7 +15,11 @@ const NavigationBar = () => {
       <NavBarItem link='/colorCodes' id='colorcodesLink' text='Color Codes'></NavBarItem>
       <NavBarItem link='/custom-top-sort' id='custom-top-sortLink' text='Custom top sort'></NavBarItem>
       <NavBarItem link='/draw-contest' id='drawcontestLink' text='Draw contest'></NavBarItem>
-      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>☀/☾</button>
+      <button type="button" className={`${button} unselectable`} onClick={darkMode.toggle}>
+        <Icon icon={Sun} className={lightDarkModeIcon} size='24px' alt='Change to light mode'></Icon>
+        <div className={lightDarkModeIcon} > / </div>
+        <Icon icon={Moon} className={lightDarkModeIcon} size='18px' alt='Change to dark mode'></Icon>
+      </button>
     </div>
   );
 };
