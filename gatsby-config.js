b2a2bcfffb8958abc75975f6109441125828b0ca
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-03-18 20:18:24 +0100

    update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://arvid.sandin.dev",
     title: "Arvid Sandin",
+    description: "Arvid Sandin's personal website",
   },
   plugins: [
     'gatsby-plugin-use-dark-mode',
