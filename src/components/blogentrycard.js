269c0d98c2a305f0850599c1145e51c5c48c5cfe
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-03-19 11:37:49 +0100

    Update text content +Fix underline on anchor hover

--- src/components/blogentrycard.js
+++ src/components/blogentrycard.js
@@ -1,12 +1,12 @@
 import React from "react"
-import { title, card, excerp, date } from './blogentrycard.module.css'
+import { title, card, excerp, date, link } from './blogentrycard.module.css'
 
 
 const BlogEntryCard = ({ data }) => {
 
     return(
-        <div className = { card }>
-            <a href={`/blog${data.frontmatter.slug}`}>
+        <div className = {card}>
+            <a className={link} href={`/blog${data.frontmatter.slug}`}>
                 <h2 className={title}>{data.frontmatter.title}</h2>
                 <p className={excerp}>{data.excerpt}</p>
                 <p className={date}>{data.frontmatter.date}</p>
