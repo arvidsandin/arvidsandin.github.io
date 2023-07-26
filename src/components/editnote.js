1a41a72127b5092224735eed2a3b81fbec5741ab
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2023-07-26 17:29:18 +0200

    Always show last edited on blog posts

--- src/components/editnote.js
+++ src/components/editnote.js
@@ -4,7 +4,7 @@ import { editNote } from './editnote.module.css'
 const EditNote = ({ edited, created }) => {
     return (
         <div className={editNote}>
-            {edited !== created && <p>Last edited: {edited}</p>}
+            {<p>Last edited: {edited || created}</p>}
         </div>
     );
 };
